0524255eec142232af9e0a5d0fa0a04864c6abf4
bilal <bilalhamdaoui2@gmail.com>
2020-09-25 16:54:17 +0200

    the Project won't star i have renamed the imported class from UserEntity to User, looks like the refactor wasn't made automatically.

--- src/main/java/com/example/framaDate/repository/UserRepository.java
+++ src/main/java/com/example/framaDate/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.framadate.repository;
 
-import com.example.framadate.entity.UserEntity;
+import com.example.framadate.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
