878d90904be63c296b96dcf6649d230b6955ef5f
bilal <bilalhamdaoui2@gmail.com>
2020-09-26 17:37:02 +0200

    Interface describing all data operation that can be made to handle comments data

--- src/main/java/com/example/framaDate/service/ICommentService.java
+++ src/main/java/com/example/framaDate/service/ICommentService.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public interface ICommentService {
 
+
+    /* get comments of a specific survey
+   params :
+   surveyID : the id of the survey of want to select it's comments. a default limit will be applied to the result !
+   */
     List<Comment> getComments(int surveyID);
 
-    List<Comment> getComments(int surveyID, int count =100, int skip);
+    /* get comments of a specific survey
+    params :
+    surveyID : the id of the survey of want to select it's comments
+    count : how many comments would you like to select
+    skip  : how many comments to skip (for pagination)
+     */
+    List<Comment> getComments(int surveyID, int count, int skip);
 
 }
