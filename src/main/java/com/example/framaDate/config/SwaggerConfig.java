eb92ec4ccba1af069f4523c558542f4973084f99
achrafJhidri <achrafjh@gmail.com>
2020-10-17 13:13:43 +0200

    package path fixed

--- src/main/java/com/example/framaDate/config/SwaggerConfig.java
+++ src/main/java/com/example/framaDate/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.framaDate"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.framadate"))
                 .paths(PathSelectors.any())
                 .build();
     }
